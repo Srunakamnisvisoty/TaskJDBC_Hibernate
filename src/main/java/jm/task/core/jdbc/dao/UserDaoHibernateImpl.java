a7d26003b952b0627b90b3ea440a6526df47c25d
Srunakamnisvisoty <konchic27041998@gmail.com>
2022-07-17 16:29:50 +0400

    1

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,122 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.HibernateError;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.NativeQuery;
 
+import java.util.Collections;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+
+    private final Util util;
+
     public UserDaoHibernateImpl() {
-
+        this.util = new Util();
     }
-
 
     @Override
     public void createUsersTable() {
-
+        String sql = "CREATE TABLE IF NOT EXISTS `user` (\n" +
+                "  `id` bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,\n" +
+                "  `name` varchar(255) COLLATE 'utf8_general_ci' NOT NULL,\n" +
+                "  `lastName` varchar(255) COLLATE 'utf8_general_ci' NOT NULL,\n" +
+                "  `age` int NOT NULL\n" +
+                ") ENGINE='InnoDB' COLLATE 'utf8_general_ci';";
+        try {
+            SessionFactory sessionFactory = util.getSession();
+            Session session = sessionFactory.openSession();
+            session.beginTransaction();
+            session.createSQLQuery(sql);
+            session.getTransaction().commit();
+            session.close();
+        } catch (HibernateException exception) {
+            exception.printStackTrace();
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        String sql = "DROP TABLE IF EXISTS user";
+        try {
+            SessionFactory sessionFactory = util.getSession();
+            Session session = sessionFactory.openSession();
+            session.beginTransaction();
+            session.createSQLQuery(sql);
+            session.getTransaction().commit();
+            session.close();
+        } catch (HibernateException exception) {
+            exception.printStackTrace();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        String sql = "INSERT INTO user (name, lastName, age) VALUES (?, ?, ?);";
+        try {
+            SessionFactory sessionFactory = util.getSession();
+            Session session = sessionFactory.openSession();
+            session.beginTransaction();
+            NativeQuery<User> nativeQuery = session.createNativeQuery(sql, User.class);
+            nativeQuery.setParameter(1, name);
+            nativeQuery.setParameter(2, lastName);
+            nativeQuery.setParameter(3, age);
+            session.getTransaction().commit();
+            session.close();
+        } catch (HibernateException exception) {
+            exception.printStackTrace();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        String sql = "DELETE FROM user WHERE id = ?";
+        try {
+            SessionFactory sessionFactory = util.getSession();
+            Session session = sessionFactory.openSession();
+            session.beginTransaction();
+            NativeQuery<User> nativeQuery = session.createNativeQuery(sql, User.class);
+            nativeQuery.setParameter(1, id);
+            session.getTransaction().commit();
+            session.close();
+        } catch (HibernateException exception) {
+            exception.printStackTrace();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        String sql = "SELECT id, name, lastName, age FROM user";
+        try {
+            SessionFactory sessionFactory = util.getSession();
+            Session session = sessionFactory.openSession();
+            session.beginTransaction();
+            NativeQuery<User> nativeQuery = session.createNativeQuery(sql, User.class);
+            session.getTransaction().commit();
+            session.close();
+            return nativeQuery.getResultList();
+        } catch (HibernateException exception) {
+            exception.printStackTrace();
+        }
+        return Collections.emptyList();
     }
 
     @Override
     public void cleanUsersTable() {
-
+        String sql = "TRUNCATE TABLE user";
+        try {
+            SessionFactory sessionFactory = util.getSession();
+            Session session = sessionFactory.openSession();
+            session.beginTransaction();
+            session.createNativeQuery(sql);
+            session.getTransaction().commit();
+            session.close();
+        } catch (HibernateException exception) {
+            exception.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,37 @@
 package jm.task.core.jdbc.util;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    public SessionFactory getSession() {
+        return createSession();
+    }
+
+    private SessionFactory createSession() {
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(setting()).build();
+        MetadataSources metadataSources = new MetadataSources(serviceRegistry);
+        Metadata metadata = metadataSources.buildMetadata();
+        return  metadata.getSessionFactoryBuilder().build();
+    }
+
+    private Map<String, String> setting() {
+        Map<String, String> map = new HashMap<>();
+        map.put("connection.driver_class", "com.mysql.cj.jdbc.Driver");
+        map.put("hibernate.connection.url", "jdbc:mysql://localhost:3306/user");
+        map.put("hibernate.connection.username", "root");
+        map.put("hibernate.connection.password", "root");
+        map.put("hibernate.show_sql", "true");
+        map.put("hibernate.format_sql", "true");
+        map.put("hibernate.current_session_context_class", "thread");
+        map.put("dialect", "org.hibernate.dialect.MySQL8Dialect");
+        return map;
+    }
 }
